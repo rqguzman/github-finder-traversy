ba06da5f591d57bf9a4e3274a5f5786b421983b3
rqguzman <rafael.guzman@outlook.com>
2021-06-04 15:57:42 -0300

    :recycle: refactor(components): replace class-based components

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,45 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-    // constructor() {
-    //     super();
-    //     this.state = {
-    //         id: 'id',
-    //         login: 'mojombo',
-    //         avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-    //         html_url: 'https://github.com/mojombo'
-    //     }
-    // }
-    // state = {
-    //     id: 'id',
-    //     login: 'mojombo',
-    //     avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-    //     html_url: 'https://github.com/mojombo'
-    // }
+const UserItem = ({ user: {login, avatar_url, html_url} }) => {
 
-    render() {
-        const { login, avatar_url, html_url } = this.props.user;
-        return (
-            <div className="card text-center">
-                <img
-                    src={avatar_url}
-                    alt="" className="round-img"
-                    style={{ width: '60px' }}
-                />
+    return (
+        <div className="card text-center">
+            <img
+                src={avatar_url}
+                alt="" className="round-img"
+                style={{ width: '60px' }}
+            />
 
-                <h3>{login}</h3>
+            <h3>{login}</h3>
 
-                <div>
-                    <a
-                        href={html_url}
-                        className="btn btn-dark btn-sm my-1"
-                    >
-                        More
+            <div>
+                <a
+                    href={html_url}
+                    className="btn btn-dark btn-sm my-1"
+                >
+                    More
                     </a>
-                </div>
             </div>
-        )
-    }
+        </div>
+    )
+};
+
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired,
 }
 
 export default UserItem;
