3f0c9defdd7c4af0271d4ee71c3a1fb2cd3c2bec
rqguzman <rafael.guzman@outlook.com>
2021-06-04 17:04:46 -0300

    :sparkles: feat(Spinner): Add spinner component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,17 +1,26 @@
-import React, { Component } from 'react'
-import UserItem from './UserItem'
+import React from 'react';
+import Spinner from '../layout/Spinner';
+import UserItem from './UserItem';
+import PropTypes from 'prop-types';
 
-export class Users extends Component {
+const Users = ({ users, loading }) => {
 
-    render() {
+    if (loading) {
+        return <Spinner />    
+    } else {
         return (
             <div style={userStyle}>
-               {this.props.users.map(user => (
-                   <UserItem key={user.id} user={user} />
-               ))} 
+                {users.map(user => (
+                    <UserItem key={user.id} user={user} />
+                ))}
             </div>
         )
     }
+}
+
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
 }
 
 const userStyle = {
