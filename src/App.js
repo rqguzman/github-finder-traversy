980b72843ff1a75707bbb98cfe92cd87c3962da6
rqguzman <rafael.guzman@outlook.com>
2021-06-07 16:00:28 -0300

    :sparkles: feat(Alert): add Alert component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { Component } from 'react';
 import './App.css';
+import Alert from './components/layout/Alert';
 import Navbar from './components/layout/Navbar';
 import Search from './components/users/Search';
 // import UserItem from './components/users/UserItem';
@@ -10,7 +11,8 @@ class App extends Component {
 
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
   // async componentDidMount() {
@@ -32,19 +34,28 @@ class App extends Component {
     this.setState({ users: [], loading: false });
   };
 
+  setAlert = (msg, type) => {
+
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
+
   render() {
 
     const { users, loading } = this.state;
-    
+
     return (
       <div className='App'>
         <Navbar title='Github Finder' icon='fab fa-github' />
 
         <div className="container">
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
